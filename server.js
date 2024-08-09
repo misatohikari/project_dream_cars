935c9c6406f072675c15ebd5e16dbe984b7eedde
misatoHikari <myoshimoto@myseneca.ca>
2024-08-09 13:17:11 -0400

    updated server.js for cors middleware

--- server.js
+++ server.js
@@ -170,6 +170,7 @@ app.prepare().then(() => {
   // CORS configuration
   const corsOptions = {
     origin: [
+      'http://localhost:3000',
       'https://project-dream-cars.onrender.com', // Use this for your production frontend URL
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
