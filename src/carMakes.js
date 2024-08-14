7234143632d93d8f55b0e8105a57b0df144c5401
AmmarGang <97449809+AmmarGang@users.noreply.github.com>
2024-08-13 23:19:57 -0400

    Update carMakes.js

--- src/carMakes.js
+++ src/carMakes.js
@@ -2,14 +2,9 @@ const carMakes = [
     "Acura", "Alfa Romeo", "Audi", "BMW", "Buick", "Cadillac", "Chevrolet", "Chrysler",
     "Dodge", "Fiat", "Ford", "Genesis", "GMC", "Honda", "Hyundai", "Infiniti", "Jaguar",
     "Jeep", "Kia", "Land Rover", "Lexus", "Lincoln", "Mazda", "Mercedes-Benz", "Mini",
-    "Mitsubishi", "Nissan", "Porsche", "Ram", "Rivian", "Saab", "Subaru", "Tesla",
+    "Mitsubishi", "Nissan", "Porsche", "Ram", "Saab", "Tesla",
     "Toyota", "Volkswagen", "Volvo", "Aston Martin", "Bentley", "Bugatti", "Ferrari", 
-    "Lamborghini", "McLaren", "Maserati", "Rolls-Royce", "Pagani", "Alfa Romeo", "Buick",
-    "Cadillac", "Chevrolet", "Chrysler", "Dodge", "Fiat", "Ford", "Genesis", "GMC", 
-    "Honda", "Hyundai", "Infiniti", "Jaguar", "Jeep", "Kia", "Land Rover", "Lexus",
-    "Lincoln", "Mazda", "Mercedes-Benz", "Mini", "Mitsubishi", "Nissan", "Porsche",
-    "Ram", "Rivian", "Saab", "Subaru", "Tesla", "Toyota", "Volkswagen", "Volvo",
-    "Pagani", "Plymouth", "Smart", "Spyker", "Suzuki", "Tesla", "Zastava"
+    "Lamborghini", "McLaren", "Maserati", "Rolls-Royce", "Pagani", "Tesla"
 ];
 
 export default carMakes;
